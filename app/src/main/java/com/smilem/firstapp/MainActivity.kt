710b65307af8353cca482890ffe25747a96b6bc1
Kyuseo Shim <cremazer@gmail.com>
2020-11-19 07:09:34 +0900

    build.gradle plugin apply 설정 변경 - 코드 수정 후 Sync 맞춰야 함. - activity_main.xml의 fragment id가 MainActivity.kt에서 import 할 수 있도록 설정되었음.

--- app/src/main/java/com/smilem/firstapp/MainActivity.kt
+++ app/src/main/java/com/smilem/firstapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.smilem.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController()
+
     }
 }
